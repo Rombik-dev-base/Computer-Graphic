a03edc95dd4edf8182d427e4ad854c81357b35e5
Rombik-dev-base <61863339+Rombik-dev-base@users.noreply.github.com>
2020-06-13 13:22:24 +0300

    Create picture.h

---
+++ Laborathory 5/picture.h
@@ -0,0 +1,44 @@
+
+//
+// Created by 1285166 on 13.06.2020.
+//
+
+#ifndef INC_5LAB_PICTURE_H
+#define INC_5LAB_PICTURE_H
+#include <string>
+#include <vector>
+struct Pixel {
+    unsigned char A;
+    unsigned char B;
+    unsigned char C;
+};
+enum ColorSpace {
+    YCbCr601,
+    RGB
+};
+enum Conversion {
+    RGB_SET = 0,
+    YCBCR_SET = 1,
+    RGB_AUTO_SIMPLE = 2,
+    YCBCR_AUTO_SIMPLE = 3,
+    RGB_AUTO = 4,
+    YCBCR_AUTO = 5
+};
+class Picture {
+public:
+    Picture();
+    ~Picture();
+    void OpenSingleFile(std::string);
+    void SaveToSingleFile(std::string);
+    void SetCS(ColorSpace) noexcept;
+    void changeBrightness(double difference, double multiple) noexcept;
+    void autoBrightness(bool type) noexcept;
+private:
+    int height;
+    int width;
+    Pixel* image;
+    ColorSpace CurrentCS;
+    std::vector<int> getBrightestandDarkest(bool avoid) noexcept;
+    bool color;
+};
+#endif //INC_5LAB_PICTURE_H
